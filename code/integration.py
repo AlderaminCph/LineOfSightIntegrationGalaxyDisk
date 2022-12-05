164b630c79a910e4889130b3db708710ef2136e6
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-12-05 19:39:01 +0300

    normalized deviation map was added

--- code/integration.py
+++ code/integration.py
@@ -1,5 +1,7 @@
 """
-Module for integration 3D luminosity density model of exponential disk and for
+Module for integration.
+
+Integrate 3D luminosity density model of exponential disk and for
 creating analytic 2D edge-on disk model using van der Cruit formulae.
 """
 # !/usr/bin/env python
@@ -13,14 +15,13 @@ from typing import List, Optional
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.integrate as integrate
+from numpy import ndarray
 from scipy.special import k1
 
 
 @dataclass
 class Point:
-    """
-    2 dimensional point
-    """
+    """2 dimensional point."""
 
     x: float
     y: float
@@ -28,9 +29,7 @@ class Point:
 
 @dataclass
 class Bounds:
-    """
-    Limits of integration.
-    """
+    """Limits of integration."""
 
     lower: float
     upper: float
@@ -38,9 +37,7 @@ class Bounds:
 
 @dataclass
 class GalaxyProperties:
-    """
-    Parameters of galaxy.
-    """
+    """Parameters of galaxy."""
 
     pos_angle: float
     central_lum_density: float
@@ -50,13 +47,17 @@ class GalaxyProperties:
 
 
 def gauss_legendre(
-        luminosity_density,
-        limits: Optional[Bounds],
-        parameters: List[float],
-        roots: List[float],
-        weights: List[float],
+    luminosity_density,
+    limits: Optional[Bounds],
+    parameters: List[float],
+    roots: List[float],
+    weights: List[float],
 ) -> float:
-    """Integrate luminosity_density within integration bounds using gauss legendre quadrature.
+    """Integrate luminosity_density.
+
+    Integration performed within integration bounds using
+    gauss legendre quadrature.
+
     Args:
         parameters: the list of parameters for integrand function
         luminosity_density: integrand function
@@ -75,19 +76,23 @@ def gauss_legendre(
 
 
 def make_model(
-        model_type: str,
-        frame_size: int,
-        parameters: List[float],
-        roots=None,
-        weights=None,
-        quad=False,
+    model_type: str,
+    frame_size: int,
+    parameters: List[float],
+    roots=None,
+    weights=None,
+    quad=False,
 ) -> List[List]:
-    """Return the 2D array with total intensity values and size of the framesize.
+    """Return the 2D array with total intensity values.
+
+     The size of an array is he frame size.
     Args:
         model_type: name of the model (exp_disk_3D or edge_on)
         frame_size: the size of the grid
-        parameters: list of function parameters p = [x0, y0, PA, inc, J_0, h, n, z_0]
-            centre_pixel: Point object with x and y pixel coordinate of the centre
+        parameters: list of function parameters
+            p = [x0, y0, PA, inc, J_0, h, n, z_0]
+            centre_pixel: Point object with x and y pixel
+                        coordinate of the centre
             PA: major-axis position angle (in degrees)
             inc: inclination with respect to the line of sight (in degrees)
             J_0: the central luminosity density
@@ -107,28 +112,37 @@ def make_model(
             if model_type == "exp_disk_3D":
                 intensity.append(
                     exponential_disk_3d(
-                        Point(x_coord, y_coord), parameters, roots, weights, quad
+                        Point(x_coord, y_coord),
+                        parameters,
+                        roots,
+                        weights,
+                        quad,
                     )
                 )
             elif model_type == "edge_on":
-                intensity.append(edge_on_disk(Point(x_coord, y_coord), parameters))
+                intensity.append(
+                    edge_on_disk(Point(x_coord, y_coord), parameters)
+                )
         total_intensity.append(intensity)
     return total_intensity
 
 
 def exponential_disk_3d(
-        pixel: Optional[Point],
-        parameters: List[float],
-        roots: List[float],
-        weights: List[float],
-        quad=False,
+    pixel: Optional[Point],
+    parameters: List[float],
+    roots: List[float],
+    weights: List[float],
+    quad=False,
 ) -> float:
-    """Get intensity of the pixel with coordinates x, y in model
-        of 3D exponential disk.
+    """Get intensity of the pixel.
+
+    Pixel with coordinates x, y in model of 3D exponential disk.
     Args:
         pixel: Point object with x and y coordinate of pixel
-        parameters: list of function parameters p = [centre_pixel, pos_angle, inc, J_0, h, n, z_0]
-            centre_pixel: Point object with x and y pixel coordinate of the centre
+        parameters: list of function parameters
+            p = [centre_pixel, pos_angle, inc, J_0, h, n, z_0]
+            centre_pixel: Point object with x and y pixel
+                        coordinate of the centre
             pos_angle: major-axis position angle (in degrees)
             inc: inclination with respect to the line of sight (in degrees)
             J_0: the central luminosity density
@@ -141,7 +155,15 @@ def exponential_disk_3d(
     Return:
         intensity: intensity of the pixel
     """
-    centre_pixel, pos_angle, inc, central_lum_density, h_scale, n, z_0 = parameters
+    (
+        centre_pixel,
+        pos_angle,
+        inc,
+        central_lum_density,
+        h_scale,
+        n,
+        z_0,
+    ) = parameters
     pos_angle += 90
     inc_rad = math.radians(inc)
     cos_inc = math.cos(inc_rad)
@@ -156,7 +178,17 @@ def exponential_disk_3d(
     y_d0 = projected_pix.y * cos_inc
     z_d0 = projected_pix.y * sin_inc
 
-    param = [x_d0, y_d0, z_d0, cos_inc, sin_inc, central_lum_density, h_scale, z_0, n]
+    param = [
+        x_d0,
+        y_d0,
+        z_d0,
+        cos_inc,
+        sin_inc,
+        central_lum_density,
+        h_scale,
+        z_0,
+        n,
+    ]
     if inc < 80:
         # intersection of the disk plane and sky plane
         s_plane = projected_pix.y * np.tan(inc_rad)
@@ -201,14 +233,17 @@ def exponential_disk_3d(
             )
         )
     else:
-        intensity = integrate.quad(get_luminosity_density, -100, 100, args=param)[0]
+        intensity = integrate.quad(
+            get_luminosity_density, -100, 100, args=param
+        )[0]
     return intensity
 
 
 def get_luminosity_density(
-        line_of_sight_coord: float, parameters: List[float]
+    line_of_sight_coord: float, parameters: List[float]
 ) -> float:
     """Get luminosity density along line of sight coordinate s.
+
     Args:
         line_of_sight_coord (float): line of sight coordinate
         parameters: list of function parameters
@@ -222,9 +257,8 @@ def get_luminosity_density(
             h: exponential scale length
             z_0: the vertical scale height
     Return:
-        lum_density: luminosity density along line of sight coordinate line_of_sight_coord.
+        lum_density: luminosity density along line of sight coordinate.
     """
-
     (
         x_d0,
         y_d0,
@@ -239,20 +273,23 @@ def get_luminosity_density(
 
     x_d = x_d0
     y_d = y_d0 + line_of_sight_coord * sin_inc
-    z_d = z_d0-line_of_sight_coord * cos_inc
+    z_d = z_d0 - line_of_sight_coord * cos_inc
 
-    r_coord = np.sqrt(x_d**2 + y_d**2)
+    r_coord = np.sqrt(x_d ** 2 + y_d ** 2)
     z_coord = abs(z_d)
 
     vertical_scaling = get_vertical_scaling(n, z_0, z_coord)
 
-    lum_density = central_lum_density * np.exp(-r_coord / h_scale) * vertical_scaling
+    lum_density = (
+        central_lum_density * np.exp(-r_coord / h_scale) * vertical_scaling
+    )
     return lum_density
 
 
-def numeric_solution(roots, weights):
+def numeric_solution(roots, weights, plot_flag: bool = False):
     """
-    Calculate numeric solution for integration of 3D exponential disk
+    Calculate numeric solution for integration of 3D exponential disk.
+
     Args:
         roots: roots of the gauss legendre polynom
         weights: weights of the gauss legendre polynom
@@ -264,17 +301,21 @@ def numeric_solution(roots, weights):
 
     parameters = setup_galaxy("exp_disk_3D")
 
-    total_intensity = make_model("exp_disk_3D", frame_size, parameters, roots, weights)
-    plot_intensity_map(total_intensity, "Numeric", "Numeric.png")
+    total_intensity = make_model(
+        "exp_disk_3D", frame_size, parameters, roots, weights
+    )
+    if plot_flag:
+        plot_intensity_map(total_intensity, "Numeric", "Numeric.png")
 
     return total_intensity
 
 
 def get_reference_frame_coords(
-        pixel: Optional[Point], centre_pixel: Optional[Point], pos_angle: float
+    pixel: Optional[Point], centre_pixel: Optional[Point], pos_angle: float
 ) -> Optional[Point]:
     """
-    Calculate coordinates in component reference frame
+    Calculate coordinates in component reference frame.
+
     Args:
         pixel: Point object with current x and y pixel coordinate
         centre_pixel: Point object with x and y pixel coordinate of the centre
@@ -296,7 +337,8 @@ def get_reference_frame_coords(
 
 def get_vertical_scaling(n: float, z_0: float, z_coord: float):
     """
-    Calculate the multiplier
+    Calculate the multiplier.
+
     Args:
         n: the vertical profile follows the sech^(2/n) function
         z_0: the vertical scale height
@@ -321,8 +363,9 @@ def get_vertical_scaling(n: float, z_0: float, z_coord: float):
 
 
 def edge_on_disk(pixel: Optional[Point], parameters: List[float]) -> float:
-    """Provides the analytic form for an edge-on
-    disk with a radial exponential profile, using the Bessel-function
+    """Provide the analytic form for an edge-on disk.
+
+    Disk model with a radial exponential profile, using the Bessel-function
     solution of van der Kruit & Searle (1981) for the radial profile.
 
         I(r,z) = mu(0,0) * (r/h) * K_1(r/h) * sech^(2/n)(n*z/(2*z0))
@@ -331,7 +374,8 @@ def edge_on_disk(pixel: Optional[Point], parameters: List[float]) -> float:
         K_1(r/h) - Modified Bessel function of the second kind of order 1
     Args:
         pixel: Point object with x and y coordinate of pixel
-        parameters: list of function parameters p = [x0, y0, PA, inc, J_0, h, n, z_0]
+        parameters: list of function parameters
+                    p = [x0, y0, PA, inc, J_0, h, n, z_0]
             x0: x pixel coordinate of the centre
             y0: y pixel coordinate of the centre
             PA: major-axis position angle (in degrees)
@@ -344,7 +388,9 @@ def edge_on_disk(pixel: Optional[Point], parameters: List[float]) -> float:
     """
     pixel_center, pos_ang, central_lum_density, h_scale, n, z_0 = parameters
 
-    projected_point = get_reference_frame_coords(pixel, pixel_center, pos_ang + 90)
+    projected_point = get_reference_frame_coords(
+        pixel, pixel_center, pos_ang + 90
+    )
     r_coord, z_coord = np.fabs(projected_point.x), np.fabs(projected_point.y)
     # Calculate r,z in coordinate system aligned with the edge-on disk
 
@@ -356,20 +402,28 @@ def edge_on_disk(pixel: Optional[Point], parameters: List[float]) -> float:
         intensity = mu_0
     else:
         intensity = (
-            mu_0 * (r_coord / h_scale) * k1(r_coord / h_scale) * vertical_scaling
+            mu_0
+            * (r_coord / h_scale)
+            * k1(r_coord / h_scale)
+            * vertical_scaling
         )
 
     return intensity
 
 
 def setup_galaxy(model_type: str, inc=90) -> List[float]:
-    """Setup galaxy parameters for 3D exponential disk and 2D Edge On disk
-    models.
+    """Setup galaxy parameters.
+
+    3D exponential disk and 2D Edge On disk models are concerned.
     Return:
         parameters: the vector of model's parameters.
     """
     gal_prop = GalaxyProperties(
-        pos_angle=0, central_lum_density=0.01, h_scale=30, n_value=1, z_0_scale=5
+        pos_angle=0,
+        central_lum_density=0.01,
+        h_scale=30,
+        n_value=1,
+        z_0_scale=5,
     )
     if model_type == "exp_disk_3D":
         parameters = [
@@ -392,7 +446,7 @@ def setup_galaxy(model_type: str, inc=90) -> List[float]:
 
 
 def plot_intensity_map(total_intensity, title, figname):
-    """Plot intensity map"""
+    """Plot intensity map."""
     plt.figure()
     plt.imshow(
         total_intensity,
@@ -411,41 +465,55 @@ def plot_intensity_map(total_intensity, title, figname):
     plt.show()
 
 
-def plot_edge_on():
+def plot_edge_on(plot_flag: bool = False) -> ndarray:
     """plot edge on model."""
     frame_size = 100  # the size of the grid
 
     parameters = setup_galaxy("edge_on")
 
     total_intensity = np.array(make_model("edge_on", frame_size, parameters))
-
-    plot_intensity_map(
-        total_intensity,
-        "Bessel-function solution EDGE-ON",
-        "EDGE_ON_ANALYTIC_SOLUTION.png",
-    )
+    if plot_flag:
+        plot_intensity_map(
+            total_intensity,
+            "Bessel-function solution EDGE-ON",
+            "EDGE_ON_ANALYTIC_SOLUTION.png",
+        )
     return total_intensity
 
 
-ROOTS, WEIGHTS = np.polynomial.legendre.leggauss(70)
-NUMERIC_INTENSITY = numeric_solution(ROOTS, WEIGHTS)
-ANALYTIC_INTENSITY = plot_edge_on()
-ax = []
-fig = plt.figure(figsize=(12, 4))
+def plot_difference_map(number_roots: int) -> None:
+    """Plot three images in row.
 
-ax.append(fig.add_subplot(1, 3, 1))
-plt.imshow(ANALYTIC_INTENSITY, origin='lower')
-ax[0].set_title("analytic")
+    Images: analytic, numeric solutions and
+    difference map regarding on various
+    number of legendre roots.
+    Args:
+        number_roots: the order of legendre polynome.
+    """
+    roots, weights = np.polynomial.legendre.leggauss(number_roots)
+    numeric_intensity = numeric_solution(roots, weights)
+    analytic_intensity: ndarray = plot_edge_on()
+    difference_map = (
+        numeric_intensity - analytic_intensity
+    ) / analytic_intensity
+    maps_list = [numeric_intensity, analytic_intensity, difference_map]
+    plt.rcParams["text.usetex"] = True
+    titles_list = (
+        f"Numeric ({number_roots = })",
+        f"Analytic ({number_roots = })",
+        r"$\frac{numeric-analytic}{analytic}$",
+    )
+    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+    cmaps = ["RdBu_r", "RdBu_r", "RdBu_r"]
+    for col in range(3):
+        ax = axs[col]
+        ax.set_title(titles_list[col])
+        pcm = ax.imshow(maps_list[col], origin="lower", cmap=cmaps[col])
+        fig.colorbar(pcm, ax=ax, shrink=0.7)
+    plt.savefig(f"oder_{number_roots}.png")
+    plt.show()
 
-ax.append(fig.add_subplot(1, 3, 2))
-ax[1].set_title("numeric")
-plt.imshow(NUMERIC_INTENSITY, origin='lower')
 
-ax.append(fig.add_subplot(1, 3, 3))
-ax[2].set_title("analytic - numeric")
-plt.imshow(ANALYTIC_INTENSITY-NUMERIC_INTENSITY, origin='lower')
-cbar = plt.colorbar()
-cbar.set_label("Brightness", rotation=270, labelpad=25)
-
-plt.savefig("python.png")
-plt.show()
+if __name__ == "__main__":
+    for i in range(90, 130, 10):
+        plot_difference_map(i)
